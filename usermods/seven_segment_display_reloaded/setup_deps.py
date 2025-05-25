75c95d88e25698e09d5806f77249cdde1599cf3b
Will Miles <will@willmiles.net>
2025-05-24 22:18:22 -0400

    usermods/*/setup_deps.py: Check lib_deps for deps
    Check the safest possible location for final information on what
    components are actually being linked in.  This demonstrates a safe
    approach that works even for out-of-tree modules.

--- usermods/seven_segment_display_reloaded/setup_deps.py
+++ usermods/seven_segment_display_reloaded/setup_deps.py
@@ -1,9 +1,10 @@
+from platformio.package.meta import PackageSpec
 Import('env')
 
 
-usermods = env.GetProjectOption("custom_usermods","").split()
+libs = [PackageSpec(lib).name for lib in env.GetProjectOption("lib_deps",[])]
 # Check for partner usermods
-if "SN_Photoresistor" in usermods:
+if "SN_Photoresistor" in libs:
     env.Append(CPPDEFINES=[("USERMOD_SN_PHOTORESISTOR")])
-if any(mod in ("BH1750_v2", "BH1750") for mod in usermods):    
+if any(mod in ("BH1750_v2", "BH1750") for mod in libs):    
     env.Append(CPPDEFINES=[("USERMOD_BH1750")])
--- usermods/usermod_v2_rotary_encoder_ui_ALT/setup_deps.py
+++ usermods/usermod_v2_rotary_encoder_ui_ALT/setup_deps.py
@@ -1,8 +1,8 @@
+from platformio.package.meta import PackageSpec
 Import('env')
 
-
-usermods = env.GetProjectOption("custom_usermods","").split()
+libs = [PackageSpec(lib).name for lib in env.GetProjectOption("lib_deps",[])]
 # Check for partner usermod
 # Allow both "usermod_v2" and unqualified syntax
-if any(mod in ("four_line_display_ALT", "usermod_v2_four_line_display_ALT") for mod in usermods):
+if any(mod in ("four_line_display_ALT", "usermod_v2_four_line_display_ALT") for mod in libs):
     env.Append(CPPDEFINES=[("USERMOD_FOUR_LINE_DISPLAY")])
